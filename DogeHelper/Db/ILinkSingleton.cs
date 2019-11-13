3763ffe886b123697524a7ed154f020a8e326ede
Squabbi <squabbi@users.noreply.github.com>
2019-11-13 20:15:23 +1100

    Interface for Links DB

---
+++ DogeHelper/Db/ILinkSingleton.cs
@@ -0,0 +1,19 @@
+﻿using DogeHelper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogeHelper.Db
+{
+    public interface ILinkSingleton
+    {
+        Task<Link> GetMessage(string key);
+        Task<int> RemoveMessage(string key);
+        void RemoveAllMessages();
+        Task<bool> AddMessage(Link link);
+        List<Link> GetAllMessages();
+
+        Task<bool> UpdateMessage(string key, string value, string username);
+    }
+}
