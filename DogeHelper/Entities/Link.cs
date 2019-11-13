edb79c59f7c9c5191ceb75858b9c25cde2ff350b
Squabbi <squabbi@users.noreply.github.com>
2019-11-13 20:17:30 +1100

    Add Link POGO

---
+++ DogeHelper/Entities/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogeHelper.Entities
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public DateTime LastChanged { get; set; }
+        public string LastChangedBy { get; set; }
+    }
+}
